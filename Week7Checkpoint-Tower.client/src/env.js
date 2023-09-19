b9e64539da2f5fef6bd276ab2f2f5931af318ba2
TylerHigby <tyhigby22@msn.com>
2023-09-19 16:46:59 -0600

    env.js

--- Week7Checkpoint-Tower.client/src/env.js
+++ Week7Checkpoint-Tower.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-3q3o0b0ptkko8y0l.us.auth0.com'
+export const clientId = '7g3WWBQOt0eT12wZpQnF80crkTGPQoJE'
+export const audience = 'https://tylerhigby.com'
